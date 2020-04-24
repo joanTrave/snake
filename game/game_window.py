548b8e0a0596a8f7c59494850b8bc0f2fde77ae4
Joan Trave <jtravegordillo@gmail.com>
2020-04-24 13:37:24 +0200

    Añadimos la snake al game window

--- game/game_window.py
+++ game/game_window.py
@@ -2,20 +2,26 @@ from typing import NoReturn
 
 import arcade
 
+from game.snake import Snake
+
 
 class GameWindow(arcade.Window):
 
     def __init__(self, width: int, height: int, title: str) -> NoReturn:
         super().__init__(width, height, title)
 
+        self.snake = None
+
         # Set the background color
         arcade.set_background_color(arcade.color.BLACK)
 
     def setup(self) -> NoReturn:
-        pass
+        self.snake: Snake = Snake()
 
     def on_draw(self) -> NoReturn:
         arcade.start_render()
+        self.snake.draw()
 
     def on_update(self, delta_time: float) -> NoReturn:
-        pass
+        self.snake.update()
+        self.snake.update_animation()
